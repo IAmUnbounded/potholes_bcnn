f62c8585dec3382ad9c95020c7f69e3a1cd32149
root <root@instance-1.c.weighty-fabric-179806.internal>
2017-10-11 15:06:39 +0000

    pappu

--- src/fv_cnn.py
+++ src/fv_cnn.py
@@ -17,11 +17,10 @@ from sklearn.datasets import make_classification
 from keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input
 def dictionary(descriptors, N):
-	em = cv2.EM(N)
+	em = cv2.ml.EM_create(N)
 	em.train(descriptors)
 
-	return np.float32(em.getMat("means")), \
-		np.float32(em.getMatVector("covs")), np.float32(em.getMat("weights"))[0]
+	return np.float32(em.getMat("means")), np.float32(em.getMatVector("covs")), np.float32(em.getMat("weights"))[0]
 
 def image_descriptors(file):
 	img = cv2.imread(file, 0)
@@ -129,68 +128,59 @@ def get_args():
     parser.add_argument('-n' , "--number", help="Number of words in dictionary" , default=5, type=int)
     args = parser.parse_args()
     return args
-
-model = VGG16(weights='imagenet', include_top=False)
-directory = '/home/sukhad/torch-feature-extract/images/*.jpg'
-filenames = glob.glob(directory)
-X = []
-Y = []
-count = 0
-for file in filenames:
-	img = image.load_img(file)
-	w,h = img.size
-	img1 = img.crop((w/4,h/4,w/2,h/2))
-	img1 = img1.resize((224,224),Image.ANTIALIAS)
-	x1 = image.img_to_array(img1)
-	x1 = np.expand_dims(x1, axis=0)
-	x1 = preprocess_input(x1)
-	features = model.predict(x1)
-	features = features.reshape(1,512*7*7)
-	features = features.tolist()
-	X.append(features[0])
-	count = count + 1
-	Y.append(-1)
-doc = etree.parse("/home/sukhad/potholes/positive.xml")
-memoryElem = doc.find('images')
-for i in memoryElem:
-	if(len(i.getchildren())>0):
-		img_path = i.get('file')
-		#print img_path
-		img = image.load_img(img_path)
-		for j in i.iter('box'):
-			y = int(j.get('top'))
-	   		x = int(j.get('left'))
-			width = int(j.get('width'))
-			height = int(j.get('height'))
-			#print x
-			#print y
-			#print width
-			#print height
-			img1 = img.crop((x,y,width,height))
-			img1 = img1.resize((224,224),Image.ANTIALIAS)
-			x1 = image.img_to_array(img1)
-			x1 = np.expand_dims(x1, axis=0)
-			x1 = preprocess_input(x1)
-			features = model.predict(x1)
-			features = features.reshape(1,512*7*7)
-			features = features.tolist()
-			#for k in range(35):
-			X.append(features[0])
-			count = count + 1
-			Y.append(1)
-with open('model.pkl', 'wb') as fid:
-    cPickle.dump(clf, fid)
-
-gmm=generate_gmm(X,N)
-	 
-fisher_features = fisher_features(working_folder, gmm)
-#TBD, split the features into training and validation
-classifier = train(gmm, fisher_features)
-rate = success_rate(classifier, fisher_features)
-print("Success rate is", rate)
-a = np.array(X)
-print a.shape
-print len(X)
-print len(Y)
-clf = RandomForestClassifier(max_depth=2, random_state=0)
-clf.fit(X,Y)
+if __name__ == '__main__':
+	model = VGG16(weights='imagenet', include_top=False)
+	directory = '/home/sukhad/dataset/negative/*.jpg'
+	filenames = glob.glob(directory)
+	X = []
+	Y = []
+	count = 0
+	for file in filenames:
+		img = image.load_img(file)
+		w,h = img.size
+		img1 = img.crop((w/4,h/4,w/2,h/2))
+		img1 = img1.resize((224,224),Image.ANTIALIAS)
+		x1 = image.img_to_array(img1)
+		x1 = np.expand_dims(x1, axis=0)
+		x1 = preprocess_input(x1)
+		features = model.predict(x1)
+		features = features.reshape(1,512*7*7)
+		features = features.tolist()
+		X.append(features[0])
+		count = count + 1
+		Y.append(0)
+		if(count==1):
+			break
+	directory = '/home/sukhad/dataset/positive/*.jpg'
+	filenames = glob.glob(directory)
+	for file in filenames:
+		img = image.load_img(file)
+		w,h = img.size
+		img1 = img1.resize((224,224),Image.ANTIALIAS)
+		x1 = image.img_to_array(img1)
+		x1 = np.expand_dims(x1, axis=0)
+		x1 = preprocess_input(x1)
+		features = model.predict(x1)
+		features = features.reshape(1,512*7*7)
+		features = features.tolist()
+		X.append(features[0])
+		count = count + 1
+		Y.append(1)
+		if(count==2):
+			break
+	N = 512*7*7
+	gmm=generate_gmm(X,N)
+		 
+	fisher_features = fisher_features(working_folder, gmm)
+	#TBD, split the features into training and validation
+	classifier = train(gmm, fisher_features)
+	with open('model.pkl', 'wb') as fid:
+	    cPickle.dump(classifier, fid)
+	rate = success_rate(classifier, fisher_features)
+	print("Success rate is", rate)
+	#a = np.array(X)
+	#print a.shape
+	#print len(X)
+	#print len(Y)
+	#clf = RandomForestClassifier(max_depth=2, random_state=0)
+	#clf.fit(X,Y)
